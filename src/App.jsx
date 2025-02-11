9d2ee315aa27ac157251b0e07f7c0bb82c99d0de
uerbzr <nigel.sibbert@gmail.com>
2025-02-11 12:29:44 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,323 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>React Minesweeper</h1>
+      <div class="container">
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+        <div class="cell">
+          <p class="symbol"></p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
